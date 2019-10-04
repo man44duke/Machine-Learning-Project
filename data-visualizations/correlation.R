5276194b68c6ce6a261a3c8567972a26f05d0d76
Michael Nicholson <man44@duke.edu>
2019-10-04 14:08:45 -0400

    Correlation Matrix

---
+++ data-visualizations/correlation.R
@@ -0,0 +1,9 @@
+library(corrplot)
+source("machine-learning/combine_data.R")
+
+data <- combine_data(prices)
+
+df <- data.frame(date=index(data), coredata(data))
+correlation <- cor(data[,1:14])
+
+corrplot(correlation)
